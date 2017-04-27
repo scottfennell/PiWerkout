fa9d85125555f71a952080107b37a28962c4a4f3
Scott Fennell <scott@sinvertical.com>
2017-04-26 21:55:57 -0600

    Update threading and add /rpm route

--- calculate_interval.py
+++ calculate_interval.py
@@ -4,7 +4,7 @@ from time import sleep
 import threading
 import time, math, threading
 
-class IntervalMontior(threading.Thread):
+class IntervalMontior():
     dist_meas = 0.00
     km_per_hour = 0
     rpm = 0
@@ -15,23 +15,13 @@ class IntervalMontior(threading.Thread):
     running = True
     cond = None
     data = None
+    history = []
     
-    def __init__(self, cond, data):
-        self.cond = cond
-        self.data = data
-        super(IntervalMontior, self).__init__()
-        
-    def run(self):
+    def __init__(self, radius):
+        self.radius = radius
         self.init_GPIO();
         self.init_interrupt()
-        while True:
-            self.calculate_speed(20)   # call this function with wheel radius as parameter
-            self.cond.acquire()
-            self.data['rpm'] = self.rpm
-            self.data['history'][time.time] = self.rpm
-            print('rpm:{0:.0f}-RPM kmh:{1:.0f}-KMH dist_meas:{2:.2f}m pulse:{3}'.format(self.rpm,self.km_per_hour,self.dist_meas,self.pulse))
-            self.cond.release()
-            sleep(0.1)
+        print('rpm:{0:.0f}-RPM kmh:{1:.0f}-KMH dist_meas:{2:.2f}m pulse:{3}'.format(self.rpm,self.km_per_hour,self.dist_meas,self.pulse))
  
     def init_GPIO(self):               # initialize GPIO
        GPIO.setmode(GPIO.BCM)
@@ -42,16 +32,21 @@ class IntervalMontior(threading.Thread):
        self.pulse+=1                        # increase pulse by 1 whenever interrupt occurred
        self.elapse = time.time() - self.start_timer      # elapse for every 1 complete rotation made!
        self.start_timer = time.time()            # let current time equals to start_timer
-
-    def calculate_speed(self, r_cm):
+       self.history.append(self.start_timer)
+       
        if self.elapse !=0:                     # to avoid DivisionByZero error
           self.rpm = 1 / self.elapse * 60
-          self.circ_cm = (2 * math.pi) * r_cm         # calculate wheel circumference in CM
+          self.circ_cm = (2 * math.pi) * self.radius         # calculate wheel circumference in CM
           self.dist_km = self.circ_cm / 100000          # convert cm to km
           self.km_per_sec = self.dist_km / self.elapse      # calculate KM/sec
           self.km_per_hour = self.km_per_sec * 3600      # calculate KM/h
           self.dist_meas = (self.dist_km * self.pulse) * 1000   # measure distance traverse in meter
-          return self.km_per_hour
+    
+    def get_rpm(self):
+        return self.rpm
+        
+    def get_history(self):
+        return self.history
 
     def init_interrupt(self):
        GPIO.add_event_detect(self.sensor, GPIO.FALLING, callback = self.calculate_elapse, bouncetime = 200)
--- server.py
+++ server.py
@@ -1,24 +1,44 @@
 from flask import Flask, render_template
 from calculate_interval import IntervalMontior
-import threading
+import math
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 # socketio = SocketIO(app)
 data = {'rpm': 0, 'history':{}}
-cond = threading.Condition()
-monitor = IntervalMontior(cond,data);
-monitor.start()
 
 @app.route('/')
 def hello_world():
-    cond.acquire()
-    rpm = data.rpm
-    cond.release()
-    return render_template('index.html', rpm=rpm)
+    return render_template('index.html', rpm=monitor.get_rpm())
     
 @app.route('/rpm/<int:since>')
 def rpm_since(since):
+    
+    rpm_per_sec = []
+    last_time = 0
+    offset_start = 0
+    step = 0
+    last_sec = 0;
+    for time in monitor.get_history():
+        if !since or time > since:
+            if last_time > 0:
+                elapsed = time - last_time # Time since the last second, 
+                rpm = 1 / elapsed * 60
+                curr_sec = math.floor(time)
+                if (curr_sec - last_sec) > 1:
+                while last_sec < curr_sec:
+                    rpm_per_sec.append(rpm)
+                    last_sec += 1
+                rpm_per_sec.append(rpm)
+                
+                last_sec = curr_sec
+                last_time = time
+            else:
+                last_sec = math.floor(time)
+                offset_start = time
+                last_time = time
+            
+    
     return 0
 
 
