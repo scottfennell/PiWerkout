b4ae651bb2f7768a1e6a8d66f64f6c488352cd11
Scott Fennell <scott@sinvertical.com>
2017-12-27 09:10:16 -0700

    Add proxy class

--- webapp/src/app/virtual/custom.ts
+++ webapp/src/app/virtual/custom.ts
@@ -1,19 +1,36 @@
 declare var AFRAME;
 declare var THREE;
 
+var proxy = new function() {
+    this.exportClass = null;
+    this.setClass = function(clss) {
+        this.exportClass = clss;
+        Object.apply(this, clss);
+    } 
+    this.isVelocityActive = () => this.exportClass.isVelocityActive();
+    this.getPositionDelta = () => this.exportClass.getPositionDelta();
+    this.getVelocityDelta = () => this.exportClass.getVelocityDelta();
+}
 
-AFRAME.registerComponent('custom-controls', {
-    isVelocityActive: function () {
-        return Math.random() < 0.25;
-    },
-    getPositionDelta: function () {
-        return new THREE.Vector3(0, 0, 0);
-    },
-    getVelocityDelta: function () {
-        return new THREE.Vector3(0, 0.1, 0);
-    }
-});
+AFRAME.registerComponent('custom-controls', proxy);
 
 export class CustomControls {
+
+    constructor() {
+        proxy.setClass(this);
+    }
+
+    isVelocityActive() {
+        return false;
+    }
+
+    getPositionDelta() {
+        return THREE.Vector3(0,0,0);
+    }
+
+    getVelocityDelta() {
+        return THREE.Vector3(0,0,0);
+    }
+
 
 }
